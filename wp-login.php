56021cf718c4d5f53b1d7190561e8f464437edf1
Brookeharris <brookeharris@mail.usf.edu>
2022-02-21 20:11:57 -0500

    update 2/21

--- wp-login.php
+++ wp-login.php
@@ -4,12 +4,48 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<title>Log In &lsaquo; Eagle Cove Bar Services &#8212; WordPress</title>
 	<meta name="robots" content="max-image-preview:large, noindex, noarchive">
+<link rel="dns-prefetch" href="//use.fontawesome.com">
 <link rel="dns-prefetch" href="//s.w.org">
-<link rel="stylesheet" id="dashicons-css" href="https://brookeharris.github.io/eaglecoveservices/wp-includes/css/dashicons.min.css?ver=6.0-alpha-52743" type="text/css" media="all">
-<link rel="stylesheet" id="buttons-css" href="https://brookeharris.github.io/eaglecoveservices/wp-includes/css/buttons.min.css?ver=6.0-alpha-52743" type="text/css" media="all">
-<link rel="stylesheet" id="forms-css" href="https://brookeharris.github.io/eaglecoveservices/wp-admin/css/forms.min.css?ver=6.0-alpha-52743" type="text/css" media="all">
-<link rel="stylesheet" id="l10n-css" href="https://brookeharris.github.io/eaglecoveservices/wp-admin/css/l10n.min.css?ver=6.0-alpha-52743" type="text/css" media="all">
-<link rel="stylesheet" id="login-css" href="https://brookeharris.github.io/eaglecoveservices/wp-admin/css/login.min.css?ver=6.0-alpha-52743" type="text/css" media="all">
+<link rel="stylesheet" id="dashicons-css" href="https://brookeharris.github.io/eaglecoveservices/wp-includes/css/dashicons.min.css?ver=6.0-alpha-52781" type="text/css" media="all">
+<link rel="stylesheet" id="buttons-css" href="https://brookeharris.github.io/eaglecoveservices/wp-includes/css/buttons.min.css?ver=6.0-alpha-52781" type="text/css" media="all">
+<link rel="stylesheet" id="forms-css" href="https://brookeharris.github.io/eaglecoveservices/wp-admin/css/forms.min.css?ver=6.0-alpha-52781" type="text/css" media="all">
+<link rel="stylesheet" id="l10n-css" href="https://brookeharris.github.io/eaglecoveservices/wp-admin/css/l10n.min.css?ver=6.0-alpha-52781" type="text/css" media="all">
+<link rel="stylesheet" id="login-css" href="https://brookeharris.github.io/eaglecoveservices/wp-admin/css/login.min.css?ver=6.0-alpha-52781" type="text/css" media="all">
+<link rel="stylesheet" id="font-awesome-official-css" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" type="text/css" media="all" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+<link rel="stylesheet" id="font-awesome-official-v4shim-css" href="https://use.fontawesome.com/releases/v5.15.4/css/v4-shims.css" type="text/css" media="all" integrity="sha384-Vq76wejb3QJM4nDatBa5rUOve+9gkegsjCebvV/9fvXlGWo4HCMR4cJZjjcF6Viv" crossorigin="anonymous">
+<style id="font-awesome-official-v4shim-inline-css" type="text/css">@font-face {
+font-family: "FontAwesome";
+font-display: block;
+src: url("https://use.fontawesome.com/releases/v5.15.4/webfonts/fa-brands-400.eot"),
+		url("https://use.fontawesome.com/releases/v5.15.4/webfonts/fa-brands-400.eot?#iefix") format("embedded-opentype"),
+		url("https://use.fontawesome.com/releases/v5.15.4/webfonts/fa-brands-400.woff2") format("woff2"),
+		url("https://use.fontawesome.com/releases/v5.15.4/webfonts/fa-brands-400.woff") format("woff"),
+		url("https://use.fontawesome.com/releases/v5.15.4/webfonts/fa-brands-400.ttf") format("truetype"),
+		url("https://use.fontawesome.com/releases/v5.15.4/webfonts/fa-brands-400.svg#fontawesome") format("svg");
+}
+
+@font-face {
+font-family: "FontAwesome";
+font-display: block;
+src: url("https://use.fontawesome.com/releases/v5.15.4/webfonts/fa-solid-900.eot"),
+		url("https://use.fontawesome.com/releases/v5.15.4/webfonts/fa-solid-900.eot?#iefix") format("embedded-opentype"),
+		url("https://use.fontawesome.com/releases/v5.15.4/webfonts/fa-solid-900.woff2") format("woff2"),
+		url("https://use.fontawesome.com/releases/v5.15.4/webfonts/fa-solid-900.woff") format("woff"),
+		url("https://use.fontawesome.com/releases/v5.15.4/webfonts/fa-solid-900.ttf") format("truetype"),
+		url("https://use.fontawesome.com/releases/v5.15.4/webfonts/fa-solid-900.svg#fontawesome") format("svg");
+}
+
+@font-face {
+font-family: "FontAwesome";
+font-display: block;
+src: url("https://use.fontawesome.com/releases/v5.15.4/webfonts/fa-regular-400.eot"),
+		url("https://use.fontawesome.com/releases/v5.15.4/webfonts/fa-regular-400.eot?#iefix") format("embedded-opentype"),
+		url("https://use.fontawesome.com/releases/v5.15.4/webfonts/fa-regular-400.woff2") format("woff2"),
+		url("https://use.fontawesome.com/releases/v5.15.4/webfonts/fa-regular-400.woff") format("woff"),
+		url("https://use.fontawesome.com/releases/v5.15.4/webfonts/fa-regular-400.ttf") format("truetype"),
+		url("https://use.fontawesome.com/releases/v5.15.4/webfonts/fa-regular-400.svg#fontawesome") format("svg");
+unicode-range: U+F004-F005,U+F007,U+F017,U+F022,U+F024,U+F02E,U+F03E,U+F044,U+F057-F059,U+F06E,U+F070,U+F075,U+F07B-F07C,U+F080,U+F086,U+F089,U+F094,U+F09D,U+F0A0,U+F0A4-F0A7,U+F0C5,U+F0C7-F0C8,U+F0E0,U+F0EB,U+F0F3,U+F0F8,U+F0FE,U+F111,U+F118-F11A,U+F11C,U+F133,U+F144,U+F146,U+F14A,U+F14D-F14E,U+F150-F152,U+F15B-F15C,U+F164-F165,U+F185-F186,U+F191-F192,U+F1AD,U+F1C1-F1C9,U+F1CD,U+F1D8,U+F1E3,U+F1EA,U+F1F6,U+F1F9,U+F20A,U+F247-F249,U+F24D,U+F254-F25B,U+F25D,U+F267,U+F271-F274,U+F279,U+F28B,U+F28D,U+F2B5-F2B6,U+F2B9,U+F2BB,U+F2BD,U+F2C1-F2C2,U+F2D0,U+F2D2,U+F2DC,U+F2ED,U+F328,U+F358-F35B,U+F3A5,U+F3D1,U+F410,U+F4AD;
+}</style>
 	<meta name="referrer" content="strict-origin-when-cross-origin">
 		<meta name="viewport" content="width=device-width">
 		</head>
@@ -80,17 +116,17 @@ var pwsL10n = {"unknown":"Password strength unknown","short":"Very weak","bad":"
 	wp.i18n.setLocaleData( localeData, domain );
 } )( "default", { "locale_data": { "messages": { "": {} } } } );
 </script>
-<script type="text/javascript" src="https://brookeharris.github.io/eaglecoveservices/wp-admin/js/password-strength-meter.min.js?ver=6.0-alpha-52743" id="password-strength-meter-js"></script>
+<script type="text/javascript" src="https://brookeharris.github.io/eaglecoveservices/wp-admin/js/password-strength-meter.min.js?ver=6.0-alpha-52781" id="password-strength-meter-js"></script>
 <script type="text/javascript" src="https://brookeharris.github.io/eaglecoveservices/wp-includes/js/underscore.min.js?ver=1.13.1" id="underscore-js"></script>
 <script type="text/javascript" id="wp-util-js-extra">
 /*  */
 var _wpUtilSettings = {"ajax":{"url":"\/wordpress-trunk\/wp-admin\/admin-ajax.php"}};
 /*  */
 </script>
-<script type="text/javascript" src="https://brookeharris.github.io/eaglecoveservices/wp-includes/js/wp-util.min.js?ver=6.0-alpha-52743" id="wp-util-js"></script>
+<script type="text/javascript" src="https://brookeharris.github.io/eaglecoveservices/wp-includes/js/wp-util.min.js?ver=6.0-alpha-52781" id="wp-util-js"></script>
 <script type="text/javascript" id="user-profile-js-extra">
 /*  */
-var userProfileL10n = {"user_id":"0","nonce":"6b7aba902e"};
+var userProfileL10n = {"user_id":"0","nonce":"b79f49c58c"};
 /*  */
 </script>
 <script type="text/javascript" id="user-profile-js-translations">
@@ -100,7 +136,7 @@ var userProfileL10n = {"user_id":"0","nonce":"6b7aba902e"};
 	wp.i18n.setLocaleData( localeData, domain );
 } )( "default", { "locale_data": { "messages": { "": {} } } } );
 </script>
-<script type="text/javascript" src="https://brookeharris.github.io/eaglecoveservices/wp-admin/js/user-profile.min.js?ver=6.0-alpha-52743" id="user-profile-js"></script>
+<script type="text/javascript" src="https://brookeharris.github.io/eaglecoveservices/wp-admin/js/user-profile.min.js?ver=6.0-alpha-52781" id="user-profile-js"></script>
 	<div class="clear"></div>
 	</body>
 	</html>
